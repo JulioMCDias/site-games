0cebf5a7ef167c64865a50723b053d5d050be8a1
Julio.Dias <julio.dias@jbq.global>
2026-01-31 20:08:52 -0300

    feat: version vite

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  min-height: 100vh;
+  background: #0f172a;
+  color: #fff;
+`
+
+export function App() {
+  return (
+    <Container>
+      <h1>React + Vite + styled-components</h1>
+    </Container>
+  )
+}
